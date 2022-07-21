09dc65023f557ca2f9b2757678a09d97b86b4682
Marvel.Codes <marvellousn561@gmail.com>
2022-07-21 12:48:10 +0100

    changed border radius on minaturecard

--- components/Card/minaturecards.tsx
+++ components/Card/minaturecards.tsx
@@ -8,7 +8,7 @@ export const MinatureCards = ({
   section: string;
 }) => {
   return (
-    <div className='bg-[#0D0D0D] flex flex-col cursor-pointer items-center justify-center rounded h-[99px]  hover:border-2 border-rose-600 '>
+    <div className='bg-[#0D0D0D] flex flex-col cursor-pointer items-center justify-center rounded-[5px] h-[99px]  hover:border-2 border-rose-600 '>
       <p className='text-[#8E8B8F]'>{section}</p>
       <p className='text-white]'>{name}</p>
       <p className='text-[#8E8B8F]'>{percentage}</p>
